0d7eb1273eaf25b3ab8e86b209f99f741c40506c
YuryShageev <schageev.yur@yandex.ru>
2022-10-30 19:57:00 +0300

    Пример

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,16 @@
 public class Main {
 
     public static void main(String[] args) {
+
+        PersonService personService = new PersonService();
         Person sarah = new Person("Sarah ", 30);
-        if (sarah.isAdult()) {
+        if (personService.isAdult(sarah)) {
             System.out.println("иди в бар");
         } else {
             System.out.println("иди в школу");
         }
         System.out.println(sarah);
-        sarah.increaseAge(-20);
+        sarah.increaseAge(20);
 
     }
 }
